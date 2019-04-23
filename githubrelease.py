455900bcaf384acd193f7717b206f4c550575eb1
Immueggpain <Immueggpain@users.noreply.github.com>
2019-04-23 19:53:42 +0800

    first version for travis upload

--- githubrelease.py
+++ githubrelease.py
@@ -1,17 +1,23 @@
 import sys
 import os
-import mimetypes
 from github import Github
 
 repo_fullname =os.environ['TRAVIS_REPO_SLUG']
 tagname =os.environ['TRAVIS_TAG']
 
-g = Github(print(os.environ['GITHUBRTOKEN']))
+g = Github(os.environ['GITHUBRTOKEN'])
 
+print('uploading to repo:',repo_fullname)
 repo = g.get_repo(repo_fullname)
+
+print('uploading to tag:',tagname)
 release = repo.get_release(tagname)
-for file in sys.argv:
-	mtype = mimetypes.guess_type(file)[0]
-	print(mtype)
-	release.upload_asset(file, '', mtype)
-	print(release)
+
+for a in release.get_assets():
+	print('existed asset:',a.name)
+	print('delete asset:',a.name)
+	a.delete_asset()
+
+for file in sys.argv[1:]:
+	print('uploading file:',file)
+	release.upload_asset(file)
